166a3a7ac4500971b4614fd2b7feab1b3bf961a1
Irisandromeda <nikitadykiy@gmail.com>
2023-02-01 22:41:51 +0200

    UILabel extension

---
+++ UP-X/UIKit + Extensions/UILabel + Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UILabel + Extension.swift
+//  UP-X
+//
+//  Created by Irisandromeda on 01.02.2023.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    convenience init(text: String, color: UIColor, font: UIFont, alignment: NSTextAlignment) {
+        self.init()
+        
+        self.text = text
+        self.textColor = color
+        self.font = font
+        self.textAlignment = alignment
+    }
+    
+}
